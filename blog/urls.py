68cf86e3d0ce71d84a8b0859b554a3936e3795e4
Kemboi-Dun <gahrmicc@gmail.com>
2023-05-03 07:51:15 +0300

    Added posts model and posts API

--- blog/urls.py
+++ blog/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from users import urls as users_urls
+from posts import urls as posts_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # browsable api path
     path('api-auth/', include('rest_framework.urls', namespace="rest_framework")),
     path('api/users/', include(users_urls)),
+    path('api/posts/', include(posts_urls)),
 ]
